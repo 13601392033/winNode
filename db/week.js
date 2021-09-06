7ea1c5659878c2f004867b57bfb6de02d4715474
mf <13601392033@163.com>
2021-09-06 17:04:38 +0800

    d

---
+++ db/week.js
@@ -0,0 +1,12 @@
+var mongoose = require("./db.js");
+let weekSchema = mongoose.Schema({
+    id: String, // 唯一标识
+    startDate: Number, // 开始时间
+    endDate: Number, // 结束时间
+    summary: String, // 总结
+    userId: String,
+    date: Number,
+})
+
+let WeekModel = mongoose.model("week", weekSchema, "week");
+module.exports = WeekModel;
